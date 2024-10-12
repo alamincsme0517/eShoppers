8f66fd17edd98ac5d0bd626fcd8a934b9d293233
Al Amin <alamincsme@gmail.com>
2024-10-12 14:59:01 +0600

    user service impl

---
+++ src/main/java/com/alamin/eshoppers/service/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package com.alamin.eshoppers.service;
+
+import com.alamin.eshoppers.domain.User;
+import com.alamin.eshoppers.dto.UserDto;
+import com.alamin.eshoppers.repository.UserRepository;
+
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository ;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void saveUser(UserDto userDto) {
+        User user = new User();
+
+        String encrypted = encryptedPassword(userDto.getPassword());
+        user.setFirstName(userDto.getFirstName());
+        user.setLastName(userDto.getLastName());
+        user.setPassword(encrypted);
+        user.setEmail(userDto.getEmail());
+        user.setUsername(userDto.getUsername());
+
+        userRepository.save(user);
+    }
+
+    private String encryptedPassword(String password) {
+        return password ;
+    }
+}
