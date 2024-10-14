992435e0123bd1b6e8ecd926e622fd755f75e836
Al Amin <alamincsme@gmail.com>
2024-10-15 00:11:48 +0600

    cart service

---
+++ src/main/java/com/alamin/eshoppers/service/CartService.java
@@ -0,0 +1,10 @@
+package com.alamin.eshoppers.service;
+
+import com.alamin.eshoppers.domain.Cart;
+import com.alamin.eshoppers.domain.User;
+
+public interface CartService {
+    Cart getCartByUser(User currentUser);
+
+    void addProductToCart(String productId, Cart cart);
+}
