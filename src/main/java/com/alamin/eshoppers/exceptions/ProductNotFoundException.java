68cb911dc43898453868c9de52183829380b4362
Al Amin <alamincsme@gmail.com>
2024-10-15 00:14:07 +0600

    product not found exception

---
+++ src/main/java/com/alamin/eshoppers/exceptions/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package com.alamin.eshoppers.exceptions;
+
+public class ProductNotFoundException extends RuntimeException{
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
